8b8b708e169b0095b53d4f7a35f4acdef278b0f1
C PINNOCK <108437506+CEPINNOCK@users.noreply.github.com>
2022-08-18 22:06:40 +0100

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <br></br>
+        <br></br>
+<h5 style={{textAlign: "center"}}>EXPENSES TRACKER &copy;</h5>
+
+
+    </div>
+    
+  )
+}
+
+
+//Git commit this project and maintain this project through Git
